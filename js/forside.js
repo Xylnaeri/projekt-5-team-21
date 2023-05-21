9a5874a134bee1e2b2dee2950657bc7217c611c0
Pentikainen <120100980+Pentikainen@users.noreply.github.com>
2023-05-21 21:43:36 +0200

    søgefelt

--- js/forside.js
+++ js/forside.js
@@ -1,10 +1,18 @@
-const button = document.getElementById("myBtn");
-const fondInfo = document.querySelector(".neye_fond_info");
-
-button.addEventListener("mouseenter", () => {
-  fondInfo.classList.add("highlight");
-});
-
-button.addEventListener("mouseleave", () => {
-  fondInfo.classList.remove("highlight");
-});
+document.addEventListener("DOMContentLoaded", function() {
+    let searchInput = document.querySelector(".search-input");
+    let searchBtn = document.querySelector(".search-btn");
+  
+    searchBtn.addEventListener("click", function(event) {
+      event.preventDefault();
+      let searchTerm = searchInput.value;
+  
+      // Udfør søgningen (du kan tilpasse dette efter dine behov)
+      if (searchTerm.toLowerCase() === "point") {
+        // Åbn "kvitteringer.html" siden
+        window.location.href = "point.html";
+      } else {
+        // Vis en fejlmeddelelse eller tag ingen handling
+        console.log("Ugyldig søgeterm");
+      }
+    });
+  });
