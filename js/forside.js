3b1ebacfbc6e120311a8baffdf0218901c0e2d0d
Pentikainen <120100980+Pentikainen@users.noreply.github.com>
2023-05-21 22:17:23 +0200

    tilpasninger

--- js/forside.js
+++ js/forside.js
@@ -2,17 +2,35 @@ document.addEventListener("DOMContentLoaded", function() {
     let searchInput = document.querySelector(".search-input");
     let searchBtn = document.querySelector(".search-btn");
   
-    searchBtn.addEventListener("click", function(event) {
-      event.preventDefault();
-      let searchTerm = searchInput.value;
+    // Array med gyldige søgetermer
+    let validSearchTerms = ["point", "kvitteringer", "profil",];
   
-      // Udfør søgningen (du kan tilpasse dette efter dine behov)
-      if (searchTerm.toLowerCase() === "point") {
-        // Åbn "kvitteringer.html" siden
-        window.location.href = "point.html";
+    function performSearch() {
+      let searchTerm = searchInput.value.toLowerCase();
+  
+      // Kontroller om søgetermen er gyldig ved at tjekke om den findes i arrayet
+      if (validSearchTerms.includes(searchTerm)) {
+        // Åbn en side baseret på søgetermen
+        window.location.href = searchTerm + ".html";
       } else {
         // Vis en fejlmeddelelse eller tag ingen handling
         console.log("Ugyldig søgeterm");
       }
+  
+      // Nulstil søgefeltet
+      searchInput.value = "";
+    }
+  
+    searchBtn.addEventListener("click", function(event) {
+      event.preventDefault();
+      performSearch();
+    });
+  
+    searchInput.addEventListener("keyup", function(event) {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        performSearch();
+      }
     });
   });
+  
