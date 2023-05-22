d52a7009fa55ec16edb629eaeef5552a0102cc18
Pentikainen <120100980+Pentikainen@users.noreply.github.com>
2023-05-22 19:19:07 +0200

    kvitteringer

--- js/menu.js
+++ js/menu.js
@@ -10,33 +10,41 @@ menuIcon.addEventListener("click", function () {
 });
 
 document.addEventListener("DOMContentLoaded", function() {
-    const pointBox = document.querySelector(".point-box");
+    const pointBoxes = document.querySelectorAll(".point-box");
   
-    pointBox.addEventListener("click", function(event) {
-      event.stopPropagation();
+    pointBoxes.forEach(function(pointBox, index) {
+      pointBox.addEventListener("click", function(event) {
+        event.stopPropagation();
   
-      const overlay = document.createElement("div");
-      overlay.classList.add("overlay");
+        const overlay = document.createElement("div");
+        overlay.classList.add("overlay");
   
-      const preview = document.querySelector("#preview1").cloneNode(true);
-      preview.classList.add("preview-overlay");
+        const preview = document.querySelector(`#preview${index + 1}`).cloneNode(true);
+        preview.classList.add("preview-overlay");
   
-      const closeBtn = preview.querySelector(".kvittering-close");
-      closeBtn.addEventListener("click", function() {
-        overlay.remove();
+        const closeBtn = preview.querySelector(".kvittering-close");
+        closeBtn.addEventListener("click", function() {
+          overlay.remove();
+        });
+  
+        overlay.appendChild(preview);
+        document.body.appendChild(overlay);
       });
+    });
   
-      overlay.appendChild(preview);
-      document.body.appendChild(overlay);
-  
-      // Lyt efter klik uden for overlayet
-      document.addEventListener("click", function(event) {
+    // Lyt efter klik uden for overlayet
+    document.addEventListener("click", function(event) {
+      const overlays = document.querySelectorAll(".overlay");
+      overlays.forEach(function(overlay) {
         if (!overlay.contains(event.target) || event.target === overlay) {
           overlay.remove();
         }
       });
     });
   });
+  
+  
+
   
   
   
