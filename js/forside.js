82eda327a8f513e17ad08f609cb4074a3200bd12
Pentikainen <120100980+Pentikainen@users.noreply.github.com>
2023-05-29 08:24:17 +0200

    kommentarer

--- js/forside.js
+++ js/forside.js
@@ -1,30 +1,33 @@
+// indlæser DOM
 document.addEventListener("DOMContentLoaded", function() {
     let searchInput = document.querySelector(".search-input");
     let searchBtn = document.querySelector(".search-btn");
-  
-    // Array med gyldige søgetermer
+
+// opretter et array
     let validSearchTerms = ["point", "kvitteringer", "profil",];
   
     function performSearch() {
       let searchTerm = searchInput.value.toLowerCase();
-  
+
+// opretter en for-løkke 
       for (let i = 0; i < validSearchTerms.length; i++) {
         if (validSearchTerms[i] === searchTerm) {
-          // Åbn en side baseret på søgetermen
           window.location.href = searchTerm + ".html";
-          return; // Exit the loop and the function
+          return;
         }
       }
-  
-      // Nulstil søgefeltet
+
+// nulstiller søgefelt
       searchInput.value = "";
     }
-  
+
+// lytter efter click
     searchBtn.addEventListener("click", function(event) {
       event.preventDefault();
       performSearch();
     });
-  
+
+// lytter efter enter tasten
     searchInput.addEventListener("keyup", function(event) {
       if (event.key === "Enter") {
         event.preventDefault();
@@ -32,4 +35,4 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     });
   });
-  
+ 
